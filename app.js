86d116125526502b4ded390b53de78c38af2dd56
Johan Commelin <johan@commelin.net>
2025-11-18 17:08:52 +0100

    Add Express app with routes

---
+++ app.js
@@ -0,0 +1,51 @@
+// Main application entry point
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.get('/users', (req, res) => {
+  res.json([]);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json({ id: 1 });
+});
+
+app.get('/products', (req, res) => {
+  res.json([]);
+});
+
+app.post('/products', (req, res) => {
+  res.status(201).json({ id: 1 });
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).send('Not found');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
